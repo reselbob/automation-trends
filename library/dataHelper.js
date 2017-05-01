54dfc31eb0046096b3739f43d3ed00c8a540b866
Bob Reselman <breselman@h2wellness.com>
2017-05-01 16:19:08 -0700

    Get revenue by sector working

--- library/dataHelper.js
+++ library/dataHelper.js
@@ -2,21 +2,73 @@
 const path = require('path');
 const fs = require('fs');
 const _ = require('lodash');
+const Promise = require('bluebird');
+const intrinioHelper = require('./intrinioHelper');
 
-module.exports.getExchangeData = function getExchangeData(exchange){
-    let filespec;
-    const dir = process.cwd();
-    if(exchange && exchange.toUpperCase() === 'NASDAQ'){
+
+let exchangeData;
+
+const getExchangeDataSync = module.exports.getExchangeDataSync = function getExchangeDataSync(){
+    if (!exchangeData) {
+        let filespec;
+        const dir = process.cwd();
         filespec = path.join(dir, '/data/nasdaq.json');
+        const nasdaq = JSON.parse(fs.readFileSync(filespec));
+
+        nasdaq.forEach(itm => {
+            itm.exchange = 'nasdaq'
+        });
+
+        filespec = path.join(dir, '/data/nyse.json');
+        const nyse = JSON.parse(fs.readFileSync(filespec));
+        nyse.forEach(itm => {
+            itm.exchange = 'nyse'
+        });
+
+        exchangeData =  _.union(nasdaq, nyse);
     }
 
-    if(exchange && exchange.toUpperCase() === 'NYSE'){
-        filespec = path.join(dir, '/data/nyse.json');
-    }
-    let json
-    if(_.isString(filespec)){
-        const contents = fs.readFileSync(filespec);
-        json = JSON.parse(contents);
-    }
-    return json;
+    return exchangeData
 }
+
+module.exports.getSectorsSync = function getSectorsSync(){
+    const data = getExchangeDataSync();
+    return _
+        .chain(data)
+        .uniqBy('Sector')
+        .map(itm =>{
+            return itm.Sector
+        })
+        .value();
+
+}
+
+const getTickersBySectorSync = module.exports.getTickersBySectorSync = function getTickersBySectorSync(sector){
+    const data = getExchangeDataSync();
+    return _.filter(data, { 'Sector': sector});
+
+}
+
+module.exports.getPerEmployeeRevenueByTickerForSector = function getPerEmployeeRevenueByTickerForSector(sector){
+    const tickers = getTickersBySectorSync(sector);
+    //put together the ticker query
+    //const dummies = ['AAPL','MSFT', 'GOOG', 'FB'];
+    const getters = _.map(tickers, ticker => {
+        const endpoint = `data_point?identifier=${ticker.Symbol}&item=employees,totalrevenue`;
+        return intrinioHelper.getData(endpoint);
+    });
+    return Promise.all(getters)
+        .then(result => {
+            //get the employee count and revenue out by ticker
+            const arr = [];
+            result.forEach(obj =>{
+                const revenue = _.find(obj.data, {item: 'totalrevenue'}).value;
+                const ticker = _.find(obj.data, {item: 'totalrevenue'}).identifier;
+                const employees = _.find(obj.data, {item: 'employees'}).value;
+
+                const revenuePerEmployee = revenue/employees;
+                arr.push({ticker, revenue, employees, revenuePerEmployee})
+            })
+            return arr;
+        })
+};
--- test/http-access-tests.js
+++ test/http-access-tests.js
@@ -1,11 +1,12 @@
 'use strict';
 const encodingHelper = require('../library/encodingHelper');
 const intrinioHelper = require('../library/intrinioHelper');
-const dataHelper = require('../library/dataHelper')
+const dataHelper = require('../library/dataHelper');
 const chai = require('chai');
 chai.should();
 const expect = require('chai').expect;
 const app = require('../index');
+const fs = require('fs');
 
 
 describe('Intrinio Access Tests', function () {
@@ -19,10 +20,53 @@ describe('Intrinio Access Tests', function () {
     });
 
     it('Can get data from dataHelper', done => {
-        let data = dataHelper.getExchangeData('nyse');
-        expect(data).to.be.an('array');
-        data = dataHelper.getExchangeData('nasdaq');
+        let data = dataHelper.getExchangeDataSync();
         expect(data).to.be.an('array');
         done();
     });
+
+    it('Can get sectors from dataHelper', done => {
+        let data = dataHelper.getSectorsSync();
+        expect(data).to.be.an('array');
+        done();
+    });
+
+    it('Can get tickers by sector from dataHelper', done => {
+        const sectors = dataHelper.getSectorsSync();
+        sectors.forEach(sector => {
+            const result = dataHelper.getTickersBySectorSync(sector)
+            expect(result).to.be.an('array');
+            console.log(JSON.stringify({sector: sector, count: result.length}))
+        })
+        done();
+    });
+
+    // it('Can get revenue per employee per Sector', done => {
+    //     const sectors = dataHelper.getSectorsSync();
+    //     sectors.forEach(sector => {
+    //         dataHelper.getPerEmployeeRevenueByTickerForSector(sector)
+    //             .then(result => {
+    //                 expect(result).to.be.an('array');
+    //                 done();
+    //             });
+    //     });
+    //
+    // });
+
+    it('Can get revenue per employee per Transportation', done => {
+        dataHelper.getPerEmployeeRevenueByTickerForSector('Transportation')
+            .then(result => {
+                expect(result).to.be.an('array');
+                console.log(result);
+                const fileSpec = '/xxx/xxxx/Documents/holds/saved-data';
+                fs.writeFile(fileSpec, JSON.stringify(result), function (err) {
+                    if (err) {
+                        return console.log(err);
+                    };
+                    done();
+                });
+
+            });
+    });
 });
+
